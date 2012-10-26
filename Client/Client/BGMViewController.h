c4bbdb2c2fc61a69c41a289a5d2d2f2094091dc5
Jaime Iñíguez Puig <jaipuig@gmail.com>
2012-10-26 23:25:05 +0200

    QREncoder changes

--- Client/Client/BGMViewController.h
+++ Client/Client/BGMViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <QuartzCore/QuartzCore.h>
 #import "LKXAApi.h"
 
 @interface BGMViewController : UIViewController <LKXAApiDelegate>
--- Client/Client/BGMViewController.m
+++ Client/Client/BGMViewController.m
@@ -5,7 +5,6 @@
 //  Created by Daniel Devesa Derksen-Staats on 26/10/12.
 //  Copyright (c) 2012 bogamari. All rights reserved.
 //
-
 #import "BGMViewController.h"
 
 @interface BGMViewController ()
